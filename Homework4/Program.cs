79a68b5726794e0872a9fb9e9fac64e88790d97e
Faeton3D <plus0503@gmail.com>
2022-04-18 15:44:10 +0300

    Задание 4

---
+++ Homework4/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+string day = Console.ReadLine();
+
+if (day == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+if (day == "2")
+{
+    Console.WriteLine("Вторник");
+}
+if (day == "3")
+{
+    Console.WriteLine("Среда");
+}
+if (day == "4")
+{
+    Console.WriteLine("Четверг");
+}
+if (day == "5")
+{
+    Console.WriteLine("Пятница");
+}
+if (day == "6")
+{
+    Console.WriteLine("Суббота");
+}
+if (day == "7")
+{
+    Console.WriteLine("Воскресение");
+}
+if (day == "6" || day == "7")
+{
+    Console.WriteLine ($"Ура, сегодня выходной!");
+}
