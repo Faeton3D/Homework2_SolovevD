27de27ad28cde7aa91ab86e1a283def4a41d49e3
Faeton3D <plus0503@gmail.com>
2022-04-21 12:14:00 +0300

    Задача 3

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -1,9 +1,14 @@
-﻿int number = 14; int a = 0;
+﻿int number = 1408; 
 
-a = number % 10;
-if (number / 100 != 0)
-{    
-    Console.WriteLine(a);
-}
-else 
+if (number <=99)
+{
     Console.WriteLine($"Третьей цифры в заданном числе нет.");
+}
+else
+{
+    while (number >= 1000)
+{
+    number /= 10;
+}
+    Console.WriteLine(number % 10);
+}    
